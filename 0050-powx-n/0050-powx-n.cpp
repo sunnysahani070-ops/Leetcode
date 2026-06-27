ab28d855028018488e591e7ee803cd0fa48b4770
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-06-27 13:57:19 +0530

    Time: 0 ms (100%), Space: 8.4 MB (92.83%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double power(double x, long long n) {
+        if (n == 0)
+            return 1;
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+
+        return x * half * half;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+
+        if (N < 0) {
+            x = 1.0 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+};
