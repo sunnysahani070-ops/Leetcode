08a0ff1432409fa3c44f628cf12a6b5771ba251d
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-06-27 13:28:29 +0530

    Time: 0 ms (100%), Space: 9.1 MB (81.64%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,40 +1,60 @@
 class Solution {
+private:
+    long long parse(string &s, int i, long long num, int sign) {
+        // Stop if end of string or not a digit
+        if (i >= s.size() || !isdigit(s[i]))
+            return num;
+
+        int digit = s[i] - '0';
+
+        // Check overflow before adding digit
+        if (sign == 1) {
+            if (num > INT_MAX / 10 ||
+                (num == INT_MAX / 10 && digit > 7))
+                return (long long)INT_MAX + 1;
+        } else {
+            if (num > (long long)INT_MAX / 10 ||
+                (num == (long long)INT_MAX / 10 && digit > 8))
+                return (long long)INT_MAX + 1;
+        }
+
+        return parse(s, i + 1, num * 10 + digit, sign);
+    }
+
 public:
     int myAtoi(string s) {
         int i = 0;
         int n = s.size();
 
         // Skip leading spaces
-        while (i < n && s[i] == ' ') {
+        while (i < n && s[i] == ' ')
             i++;
-        }
 
-        // Handle sign
+        // Determine sign
         int sign = 1;
         if (i < n && (s[i] == '+' || s[i] == '-')) {
-            if (s[i] == '-') {
+            if (s[i] == '-')
                 sign = -1;
-            }
             i++;
         }
 
-        long long result = 0;
+        long long num = parse(s, i, 0, sign);
 
-        // Process digits
-        while (i < n && isdigit(s[i])) {
-            result = result * 10 + (s[i] - '0');
-
-            // Handle overflow
-            if (sign * result > INT_MAX) {
+        // Clamp result
+        if (sign == 1) {
+            if (num > INT_MAX)
                 return INT_MAX;
-            }
-            if (sign * result < INT_MIN) {
+            return (int)num;
+        } else {
+            if (num > (long long)INT_MAX + 1)
                 return INT_MIN;
-            }
 
-            i++;
+            long long result = -num;
+
+            if (result < INT_MIN)
+                return INT_MIN;
+
+            return (int)result;
         }
-
-        return sign * result;
     }
 };
