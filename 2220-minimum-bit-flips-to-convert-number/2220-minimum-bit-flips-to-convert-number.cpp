824408772aaffaeadc45cfa6ce623815cc1dbf75
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-06-30 16:37:13 +0530

    Time: 0 ms (100%), Space: 7.9 MB (59.19%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count=0;
+        while(start>0 || goal>0){
+            if((start%2)^(goal%2)==1){
+                count++;
+            }
+            start=start/2;
+            goal=goal/2;
+            
+        };
+        return count;
+    }
+};
