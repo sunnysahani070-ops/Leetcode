0f81c73a0a807eb44904c20f33251482b53ccf2a
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-06-30 18:03:37 +0530

    Time: 1 ms (44.1%), Space: 9.8 MB (82.17%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,21 @@
 class Solution {
-    void subset(vector<int>& arr, int i, int n, vector<int>& temp,
-                vector<vector<int>>& ans) {
-        if (i == n) {
-            ans.push_back(temp);
-            return;
-        };
-
-        temp.push_back(arr[i]);
-        subset(arr, i + 1, n, temp, ans);
-        temp.pop_back();
-        subset(arr, i + 1, n, temp, ans);
-    };
-
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> temp;
+        int n = nums.size();
         vector<vector<int>> ans;
-        subset(nums, 0, nums.size(), temp, ans);
+
+        for (int mask = 0; mask < (1 << n); mask++) {
+            vector<int> subset;
+
+            for (int j = 0; j < n; j++) {
+                if (mask & (1 << j)) {
+                    subset.push_back(nums[j]);
+                }
+            }
+
+            ans.push_back(subset);
+        }
+
         return ans;
     }
 };
