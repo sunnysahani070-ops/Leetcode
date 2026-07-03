70b5af4ee69ae5c110cfa68d6839c27878bc8268
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-07-03 13:51:24 +0530

    Time: 15 ms (65.32%), Space: 57 MB (82.5%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,24 +10,52 @@
  */
 class Solution {
 public:
+    ListNode* findMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* merge(ListNode* left, ListNode* right) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (left && right) {
+            if (left->val <= right->val) {
+                tail->next = left;
+                left = left->next;
+            } else {
+                tail->next = right;
+                right = right->next;
+            }
+            tail = tail->next;
+        }
+
+        if (left)
+            tail->next = left;
+        else
+            tail->next = right;
+
+        return dummy.next;
+    }
+
     ListNode* sortList(ListNode* head) {
-        vector<int> ls;
-        ListNode* temp=head;
-        while(temp){
-            ls.push_back(temp->val);
-            temp=temp->next;
-        }
-        temp=head;
-        sort(ls.begin(),ls.end());
-        int i=0;
-        while(i<ls.size()){
-            temp->val=ls[i];
-            temp=temp->next;
-            i++;
+        if (!head || !head->next)
+            return head;
 
-        }
-        return head;
+        ListNode* mid = findMid(head);
+        ListNode* right = mid->next;
+        mid->next = nullptr;
 
-        
+        ListNode* left = sortList(head);
+        right = sortList(right);
+
+        return merge(left, right);
     }
 };
