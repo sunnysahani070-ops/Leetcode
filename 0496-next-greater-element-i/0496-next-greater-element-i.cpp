f86fe33d9a9b02172b4cdfc2ead36592dc5f1550
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-07-04 23:45:31 +0530

    Time: 1 ms (53.43%), Space: 12.8 MB (62.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, int> nge;
         stack<int> st;
 
-        // Find next greater for every element in nums2
+        
         for (int i = nums2.size() - 1; i >= 0; i--) {
             while (!st.empty() && st.top() <= nums2[i]) {
                 st.pop();
@@ -14,7 +14,7 @@ public:
             st.push(nums2[i]);
         }
 
-        // Answer queries for nums1
+        
         vector<int> ans;
         for (int x : nums1) {
             ans.push_back(nge[x]);
