47479a28c2b610b37f3c4c607cca9f5755184e42
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-07-28 15:09:40 +0530

    Time: 35 ms (34.18%), Space: 27 MB (53.85%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        vector<unsigned long long> dp(m + 1, 0);
-
+    int fmemo(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
+        if (j < 0) return 1;
+        if (i < 0) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
         
-        dp[0] = 1;
+        if (s[i] == t[j])
+            return dp[i][j] = fmemo(i - 1, j - 1, s, t, dp) + fmemo(i - 1, j, s, t, dp);
+        else
+            return dp[i][j] = fmemo(i - 1, j, s, t, dp);
+    }
+
+    int ftab(int n, int m, string& s, string& t) {
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
 
         for (int i = 1; i <= n; i++) {
-            
-            for (int j = m; j >= 1; j--) {
-                if (s[i - 1] == t[j - 1]) {
-                    dp[j] += dp[j - 1];
-                }
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) 
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                else 
+                    dp[i][j] = dp[i - 1][j];
             }
         }
+        return dp[n][m];
+    }
 
-        return dp[m];
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return fmemo(n - 1, m - 1, s, t, dp);
     }
 };
