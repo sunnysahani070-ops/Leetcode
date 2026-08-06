e4455eda996fd37c7bcec21e230233247a464994
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-06 14:33:00 +0530

    Time: 8 ms (25.03%), Space: 9.6 MB (93.32%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,24 @@
+#include <string>
+
+class Solution {
+public:
+    int repeatedStringMatch(std::string a, std::string b) {
+        std::string s = "";
+        int count = 0;
+        while (s.length() < b.length()) {
+            s += a;
+            count++;
+        }
+        if (s.find(b) != std::string::npos) {
+            return count;
+        }
+        s += a;
+        count++;
+        
+        if (s.find(b) != std::string::npos) {
+            return count;
+        }
+        
+        return -1;
+    }
+};
