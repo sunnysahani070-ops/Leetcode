ff4294d97f19f0f33d23b4abd4a1fb05ac1c485f
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-24 23:21:12 +0530

    Time: 0 ms (100%), Space: 28.3 MB (14.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        // Optimizes standard I/O operations for faster execution times on LeetCode
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
         int count = 0;
         int candidate = 0;
-
+        
         for (int num : nums) {
             if (count == 0) {
                 candidate = num;
             }
-
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
-            }
+            // Condenses the if/else into a single line for cleaner reading
+            count += (num == candidate) ? 1 : -1; 
         }
-
+        
         return candidate;
     }
 };
