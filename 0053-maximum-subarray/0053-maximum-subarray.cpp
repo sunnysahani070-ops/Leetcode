63c743963618afebf0090448f98cb9b69b961da0
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-25 00:03:16 +0530

    Time: 0 ms (100%), Space: 71.6 MB (81.48%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        int maxSum = nums[0];
         int currentSum = 0;
         
-        for (int i = 0; i < nums.size(); i++) {
-            currentSum += nums[i];
-            
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-            }
-            
-            if (currentSum < 0) {
-                currentSum = 0;
-            }
+        for (int num : nums) {
+            currentSum = max(num, currentSum + num);
+            maxSum = max(maxSum, currentSum);
         }
         
         return maxSum;
