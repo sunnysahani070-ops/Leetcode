aae2c544428b4c5fe7daff38143629ac09d863c6
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-25 00:26:50 +0530

    Time: 0 ms (100%), Space: 32 MB (12.9%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        int max1 = -1001, max2 = -1001, max3 = -1001;
+        int min1 = 1001, min2 = 1001;
+        
+        for (int num : nums) {
+            if (num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (num > max3) {
+                max3 = num;
+            }
+            
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2) {
+                min2 = num;
+            }
+        }
+        
+        return max(max1 * max2 * max3, min1 * min2 * max1);
+    }
+};
