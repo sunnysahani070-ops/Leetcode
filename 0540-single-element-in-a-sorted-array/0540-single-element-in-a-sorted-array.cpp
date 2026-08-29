11aa51396eee1cac2b52181d522802606ac60802
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-29 16:05:52 +0530

    Time: 0 ms (100%), Space: 26.3 MB (37.12%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -6,8 +6,6 @@ public:
 
         while (low < high) {
             int mid = low + (high - low) / 2;
-
-            // Make mid even
             if (mid % 2 == 1) mid--;
 
             if (nums[mid] == nums[mid + 1]) {
