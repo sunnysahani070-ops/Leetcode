9e749454b1b397b967c5467dc15931ce8bde61b5
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-30 16:12:25 +0530

    Time: 0 ms (100%), Space: 92 MB (58.82%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int p = -1;
-        int q = -1;
-        int mx = INT_MIN;
-        int mn = INT_MAX;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         int n = nums.size();
+        if (n <= 2) return n; 
+
+        int min_idx = 0;
+        int max_idx = 0;
         
-        for(int i = 0; i < n; i++){
-            if(nums[i] > mx){
-                mx = nums[i];
-                p = i;
-            }
-            if(nums[i] < mn){
-                mn = nums[i];
-                q = i;
+        for(int i = 1; i < n; i++) {
+            if (nums[i] < nums[min_idx]) {
+                min_idx = i;
+            } else if (nums[i] > nums[max_idx]) {
+                max_idx = i;
             }
         }
         
-        int left = min(p, q);
-        int right = max(p, q);
+        int left = min(min_idx, max_idx);
+        int right = max(min_idx, max_idx);
         
-        int both_front = right + 1;
-        
-        int both_back = n - left;
-        
-        int front_and_back = (left + 1) + (n - right);
-        
-        int ans = min({both_front, both_back, front_and_back});
-        
-        return ans;
+        return min({
+            right + 1,                
+            n - left,                  
+            (left + 1) + (n - right)  
+        });
     }
 };
