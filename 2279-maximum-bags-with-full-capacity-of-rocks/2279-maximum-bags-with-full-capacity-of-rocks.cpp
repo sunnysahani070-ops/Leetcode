1ff501cedae9d8f66b5b3f5c6018df04fac4ffb1
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-08-30 18:36:21 +0530

    Time: 8 ms (97.17%), Space: 88.5 MB (95.96%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -1,6 +1,11 @@
+#pragma GCC optimize("O3", "unroll-loops")
+
 class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
         int n = capacity.size();
         
         for (int i = 0; i < n; ++i) {
