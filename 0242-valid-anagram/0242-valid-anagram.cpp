af4e857a06663ddbf505d3a870b309a5dc912bf5
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-09-01 15:41:49 +0530

    Time: 0 ms (100%), Space: 9.6 MB (92.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) {
+        if (s.length() != t.length()) {
             return false;
         }
-
-        int freqs[26] = {0};
-        int freqt[26] = {0};
-
-        for (char ch : s) {
-            freqs[ch - 'a']++;
+        
+        int count[26] = {0};
+        
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
-
-        for (char ch : t) {
-            freqt[ch - 'a']++;
-        }
-
+        
         for (int i = 0; i < 26; i++) {
-            if (freqs[i] != freqt[i]) {
+            if (count[i] != 0) {
                 return false;
             }
         }
-
+        
         return true;
     }
 };
