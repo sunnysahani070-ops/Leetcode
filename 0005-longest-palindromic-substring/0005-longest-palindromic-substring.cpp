0abed9ce3c75efaa0f266bad257585d59e2d123b
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-09-02 12:02:42 +0530

    Time: 11 ms (57.15%), Space: 9.2 MB (91.94%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -4,10 +4,7 @@ public:
         int start = 0, maxLen = 1;
 
         for (int i = 0; i < s.size(); i++) {
-            // odd length
             expand(s, i, i, start, maxLen);
-
-            // even length
             expand(s, i, i + 1, start, maxLen);
         }
 
