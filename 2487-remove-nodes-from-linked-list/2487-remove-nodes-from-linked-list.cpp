335426a88996ff3d8ed203b98dbcde2a8dee7fe2
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-09-06 16:10:12 +0530

    Time: 20 ms (31.63%), Space: 168.8 MB (50.19%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -1,28 +1,14 @@
 class Solution {
-private:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        while (head != nullptr) {
-            ListNode* nextNode = head->next;
-            head->next = prev;
-            prev = head;
-            head = nextNode;
-        }
-        return prev;
-    }
-
 public:
     ListNode* removeNodes(ListNode* head) {
-        head = reverseList(head);
-        
-        ListNode* current = head;
-        while (current != nullptr && current->next != nullptr) {
-            if (current->next->val < current->val) {
-                current->next = current->next->next;
-            } else {
-                current = current->next;
-            }
+        if (head == nullptr || head->next == nullptr) {
+            return head;
         }
-        return reverseList(head);
+        
+        head->next = removeNodes(head->next);
+        if (head->next != nullptr && head->val < head->next->val) {
+            return head->next;
+        }
+        return head;
     }
 };
