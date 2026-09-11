bf6150f80b07ce95379e006248354e1596589c81
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-09-11 17:12:56 +0530

    Time: 0 ms (100%), Space: 8.2 MB (74.63%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.length();
+        int pushes = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            pushes += (i / 8) + 1;
+        }
+        
+        return pushes;
+    }
+};
