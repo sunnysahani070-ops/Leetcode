8f3e6234a26e3511d17176f78d23a49bfcbbe716
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-09-14 00:16:43 +0530

    Time: 407 ms (50.47%), Space: 93.5 MB (25.95%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -13,7 +13,6 @@ public:
 
 private:
     void backtrack(const string& num, long target, int index, long currentVal, long prevNum, string path, vector<string>& result) {
-        // Base case: reached the end of the string
         if (index == num.length()) {
             if (currentVal == target) {
                 result.push_back(path);
@@ -22,23 +21,17 @@ private:
         }
 
         for (int i = index; i < num.length(); ++i) {
-            // Prevent numbers with leading zeros (e.g., "05")
             if (i > index && num[index] == '0') break;
 
             string part = num.substr(index, i - index + 1);
             long curr = stol(part);
 
-            // If it's the first number in the expression
             if (index == 0) {
                 backtrack(num, target, i + 1, curr, curr, part, result);
             } else {
-                // Addition
                 backtrack(num, target, i + 1, currentVal + curr, curr, path + "+" + part, result);
                 
-                // Subtraction
                 backtrack(num, target, i + 1, currentVal - curr, -curr, path + "-" + part, result);
-                
-                // Multiplication: Adjust for operator precedence by undoing the previous operation
                 backtrack(num, target, i + 1, currentVal - prevNum + (prevNum * curr), prevNum * curr, path + "*" + part, result);
             }
         }
