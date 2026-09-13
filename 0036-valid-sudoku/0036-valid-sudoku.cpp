1d269773cedf067107ed28c8f3b09d3a172b0612
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-09-14 00:12:58 +0530

    Time: 0 ms (100%), Space: 22 MB (61.31%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool rows[9][9] = {false};
+        bool cols[9][9] = {false};
+        bool boxes[9][9] = {false};
+        
+        for (int r = 0; r < 9; ++r) {
+            for (int c = 0; c < 9; ++c) {
+                if (board[r][c] != '.') {
+                    int num = board[r][c] - '1';
+                    int boxIdx = (r / 3) * 3 + (c / 3);
+                    if (rows[r][num] || cols[c][num] || boxes[boxIdx][num]) {
+                        return false;
+                    }
+                    
+                    rows[r][num] = true;
+                    cols[c][num] = true;
+                    boxes[boxIdx][num] = true;
+                }
+            }
+        }
+        
+        return true; 
+    }
+};
