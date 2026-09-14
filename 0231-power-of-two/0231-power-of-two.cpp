28b41105590554cc96d5ccbd1e44ea4bda38ac3a
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-09-14 23:35:16 +0530

    Time: 3 ms (3.35%), Space: 7.9 MB (50.4%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n>0 &&((n&(n-1))==0);
-        
+        if (n > 0 && (n & (n - 1)) == 0) {
+            return true;
+        }
+        return false;
     }
 };
