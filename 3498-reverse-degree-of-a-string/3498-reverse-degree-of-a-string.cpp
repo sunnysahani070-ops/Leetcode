e9c57ddf78f1549cc87eb31a38c2a30581252753
sunnysahani070-ops <sunnysahani070@gmail.com>
2026-09-20 17:16:50 +0530

    Time: 4 ms (9.52%), Space: 9.5 MB (98.59%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
-    int reverseDegree(string s) {
-        int sum=0;
-        for(int i=0;i<s.size();i++){
+    int reverseDegree(const string& s) {
+        int sum = 0;
+        int n = s.length();
+        for(int i = 0; i < n; ++i) {
             sum += (26 - (s[i] - 'a')) * (i + 1);
-
         }
         return sum;
-        
     }
 };
